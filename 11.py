eabce0a5847f1f1007eb8764fdb31b7d8b4fa73a
Kun Wu <zedwk@outlook.com>
2021-10-22 20:03:33 +0800

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,5 +1,11 @@
 import os
 
 example_path = os.path.join('.', 'example_mani_skill_data')
-for file in os.listdir(example_path):
-    z = 1
+for file_name in os.listdir(example_path):
+    if file_name.find("README") >= 0:
+        continue
+    
+    tar_name = file_name.split('=')[-1]
+    cur_path = os.path.join(example_path, file_name)
+    tar_path = os.path.join(example_path, tar_name)
+    os.rename(cur_path, tar_path)
