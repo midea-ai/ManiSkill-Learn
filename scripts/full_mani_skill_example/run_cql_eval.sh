0cd6b82df3a65b393726ceecb5fb3046134d5cdb
Kun Wu <zedwk@outlook.com>
2021-10-26 18:22:15 +0800

    Update run_cql_eval.sh

--- scripts/full_mani_skill_example/run_cql_eval.sh
+++ scripts/full_mani_skill_example/run_cql_eval.sh
@@ -21,7 +21,7 @@ model_list=$(python -c "import mani_skill, os, os.path as osp; print(osp.abspath
 python -m tools.run_rl configs/cql/mani_skill_point_cloud_transformer.py --evaluation --gpu-ids=3 \
 --work-dir=./test/cql_transformer_drawer/ \
 --resume-from ./work_dirs/cql_transformer_drawer/CQL/models/model_500.ckpt \
---cfg-options "env_cfg.env_name=OpenCabinetDrawer-v0" "eval_cfg.num=300" "eval_cfg.num_procs=1" "eval_cfg.use_log=True" "eval_cfg.save_traj=False"
+--cfg-options "env_cfg.env_name=OpenCabinetDrawer-v0" "eval_cfg.num=10" "eval_cfg.num_procs=1" "eval_cfg.use_log=True" "eval_cfg.save_traj=False"
 
 # python -m tools.run_rl configs/cql/mani_skill_point_cloud_transformer.py --gpu-ids=1 \
 # 	--work-dir=./work_dirs/cql_transformer_door/ \
@@ -32,7 +32,7 @@ python -m tools.run_rl configs/cql/mani_skill_point_cloud_transformer.py --evalu
 python -m tools.run_rl configs/cql/mani_skill_point_cloud_transformer.py --evaluation --gpu-ids=3 \
 --work-dir=./test/cql_transformer_door/ \
 --resume-from ./work_dirs/cql_transformer_door/CQL/models/model_500.ckpt \
---cfg-options "env_cfg.env_name=OpenCabinetDoor-v0" "eval_cfg.num=300" "eval_cfg.num_procs=1" "eval_cfg.use_log=True" "eval_cfg.save_traj=False"
+--cfg-options "env_cfg.env_name=OpenCabinetDoor-v0" "eval_cfg.num=10" "eval_cfg.num_procs=1" "eval_cfg.use_log=True" "eval_cfg.save_traj=False"
 
 # python -m tools.run_rl configs/cql/mani_skill_point_cloud_transformer.py --gpu-ids=2 \
 # 	--work-dir=./work_dirs/cql_transformer_chair/ \
@@ -43,7 +43,7 @@ python -m tools.run_rl configs/cql/mani_skill_point_cloud_transformer.py --evalu
 python -m tools.run_rl configs/cql/mani_skill_point_cloud_transformer.py --evaluation --gpu-ids=3 \
 --work-dir=./test/cql_transformer_chair/ \
 --resume-from ./work_dirs/cql_transformer_chair/CQL/models/model_500.ckpt \
---cfg-options "env_cfg.env_name=PushChair-v0" "eval_cfg.num=300" "eval_cfg.num_procs=1" "eval_cfg.use_log=True" "eval_cfg.save_traj=False"
+--cfg-options "env_cfg.env_name=PushChair-v0" "eval_cfg.num=10" "eval_cfg.num_procs=1" "eval_cfg.use_log=True" "eval_cfg.save_traj=False"
 
 # python -m tools.run_rl configs/cql/mani_skill_point_cloud_transformer.py --gpu-ids=0 \
 # 	--work-dir=./work_dirs/cql_transformer_bucket/ \
@@ -54,4 +54,4 @@ python -m tools.run_rl configs/cql/mani_skill_point_cloud_transformer.py --evalu
 python -m tools.run_rl configs/cql/mani_skill_point_cloud_transformer.py --evaluation --gpu-ids=3 \
 --work-dir=./test/cql_transformer_bucket/ \
 --resume-from ./work_dirs/cql_transformer_bucket/CQL/models/model_500.ckpt \
---cfg-options "env_cfg.env_name=MoveBucket-v0" "eval_cfg.num=300" "eval_cfg.num_procs=1" "eval_cfg.use_log=True" "eval_cfg.save_traj=False"
+--cfg-options "env_cfg.env_name=MoveBucket-v0" "eval_cfg.num=10" "eval_cfg.num_procs=1" "eval_cfg.use_log=True" "eval_cfg.save_traj=False"
