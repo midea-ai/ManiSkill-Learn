2a9754a963642ef2f5e3f54d0aa6de351fe6097d
Kun Wu <zedwk@outlook.com>
2021-10-26 09:56:10 +0800

    update cql

--- 11.py
+++ 11.py
@@ -4,7 +4,7 @@ example_path = os.path.join('.', 'example_mani_skill_data')
 for file_name in os.listdir(example_path):
     if file_name.find("README") >= 0:
         continue
-    
+
     tar_name = file_name.split('=')[-1]
     cur_path = os.path.join(example_path, file_name)
     tar_path = os.path.join(example_path, tar_name)
--- scripts/full_mani_skill_example/eval_bc_example.sh
+++ scripts/full_mani_skill_example/eval_bc_example.sh
@@ -4,6 +4,12 @@ python -m tools.run_rl configs/bc/mani_skill_point_cloud_transformer.py --evalua
 --resume-from ./full_mani_skill_data/models/OpenCabinetDoor-v0_PN_Transformer.ckpt \
 --cfg-options "env_cfg.env_name=OpenCabinetDoor-v0" "eval_cfg.num=300" "eval_cfg.num_procs=1" "eval_cfg.use_log=True" "eval_cfg.save_traj=False"
 
+# simple_mani_skill_example
+python -m tools.run_rl configs/bc/mani_skill_point_cloud_transformer.py --gpu-ids=0 --evaluation \
+--work-dir=./test/OpenCabinetDrawer_1045_link_0-v0_pcd \
+--resume-from=./example_mani_skill_data/OpenCabinetDrawer_1045_link_0-v0_PN_Transformer.ckpt \
+--cfg-options "env_cfg.env_name=OpenCabinetDrawer_1045_link_0-v0" "eval_cfg.save_video=True" "eval_cfg.num=100" "eval_cfg.num_procs=10" "eval_cfg.use_log=True"
+
 python -m tools.run_rl configs/bc/mani_skill_point_cloud_transformer.py --evaluation --gpu-ids=0 \
 --work-dir=./test/bc_transformer_drawer/ \
 --resume-from ./full_mani_skill_data/models/OpenCabinetDrawer-v0_PN_Transformer.ckpt \
