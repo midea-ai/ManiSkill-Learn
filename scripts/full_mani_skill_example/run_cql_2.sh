fbe0f7bfd4038eb31fa1762fba1f74a7d60a698b
Kun Wu <zedwk@outlook.com>
2021-10-26 11:37:24 +0800

    cql 4 tasks

--- scripts/full_mani_skill_example/run_cql_2.sh
+++ scripts/full_mani_skill_example/run_cql_2.sh
@@ -12,8 +12,9 @@ model_list=$(python -c "import mani_skill, os, os.path as osp; print(osp.abspath
 # 	"train_mfrl_cfg.init_replay_with_split=[\"./full_mani_skill_data/OpenCabinetDrawer/\",\"$model_list\"]" \
 # 	"env_cfg.env_name=OpenCabinetDrawer-v0" "eval_cfg.num=300" "eval_cfg.num_procs=1" "train_mfrl_cfg.n_eval=150000"
 
-python -m tools.run_rl configs/cql/mani_skill_point_cloud_transformer.py --gpu-ids=0 \
+
+python -m tools.run_rl configs/cql/mani_skill_point_cloud_transformer.py --gpu-ids=1 \
 	--work-dir=./work_dirs/cql_transformer_drawer/ \
 	--cfg-options "train_mfrl_cfg.total_steps=150000" "train_mfrl_cfg.init_replay_buffers=" \
-	"train_mfrl_cfg.init_replay_with_split=[\"./full_mani_skill_data/OpenCabinetDrawer/\",\"$model_list\"]" \
-	"env_cfg.env_name=OpenCabinetDrawer-v0" "eval_cfg.num=100" "eval_cfg.num_procs=1" "train_mfrl_cfg.n_eval=500"
+	"train_mfrl_cfg.init_replay_with_split=[\"./full_mani_skill_data/OpenCabinetDoor/\",\"$model_list\"]" \
+	"env_cfg.env_name=OpenCabinetDoor-v0" "eval_cfg.num=100" "eval_cfg.num_procs=1" "train_mfrl_cfg.n_eval=5000"
