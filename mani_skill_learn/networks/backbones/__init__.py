e3fb6f604a1edb2c88dab542e2e7b416733940e3
Kun Wu <zedwk@outlook.com>
2021-11-01 02:39:05 -0400

    Update __init__.py

--- mani_skill_learn/networks/backbones/__init__.py
+++ mani_skill_learn/networks/backbones/__init__.py
@@ -3,3 +3,4 @@ from .pointnet import PointNetV0
 from .vae import CVAE
 from .transformer import TransformerEncoder
 from .point_transformer_utils import index_points, square_distance
+from .point_transformer import PointTransformerBackbone, PointTransformerManiV0
